91ed32d9ca716562e54e9e57133e5523dcecc01f
zesik1 <overdrivemono@gmail.com>
2022-11-18 03:13:11 +0200

    adding a song

--- src/main/java/com/rst/radiostationdata/RadioController.java
+++ src/main/java/com/rst/radiostationdata/RadioController.java
@@ -3,14 +3,34 @@ package com.rst.radiostationdata;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class RadioController  {
 
+    List<Song> submittedSongs = new ArrayList<>();
+
+
     @GetMapping("/songs")
     public String getSongs(Model model){
-        Song song = new Song("Bon Jovi","remedies",100d);
-        model.addAttribute("song",song);
+        model.addAttribute("song",submittedSongs);
         return "songs";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitSong(Song song){
+        submittedSongs.add(song);
+        return "redirect:/songs";
+    }
+
+    @GetMapping("/")
+    public String songForm(Model model){
+        Song song = new Song();
+        model.addAttribute("song",song);
+        return "form";
+
     }
 }
