1b62e62564d1784cd979d326f1917daa8e30a9a0
zesik1 <overdrivemono@gmail.com>
2022-11-18 02:01:59 +0200

    Initial commit

---
+++ src/main/java/com/rst/radiostationdata/RadiostationDataApplication.java
@@ -0,0 +1,13 @@
+package com.rst.radiostationdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiostationDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadiostationDataApplication.class, args);
+	}
+
+}
